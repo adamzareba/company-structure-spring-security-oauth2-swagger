1a5db8e3708dd442d7006921fbdfe7d17b6af504
Adam Zaręba <bontade88@gmail.com>
2018-03-20 19:45:19 +0100

    fix

--- src/main/java/com/adamzareba/spring/security/oauth2/swagger/Application.java
+++ src/main/java/com/adamzareba/spring/security/oauth2/swagger/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
 
-@SpringBootApplication(exclude = JmxAutoConfiguration.class)
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
